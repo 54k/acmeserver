7f4ad1de36265f13e54c4cd7f69ecb76473d96a4
bkorotaev <54k@bk.ru>
2015-01-28 21:58:16 +0500

    fixed compilation

--- server/src/main/java/com/acme/server/manager/PositionManager.java
+++ server/src/main/java/com/acme/server/manager/PositionManager.java
@@ -6,7 +6,6 @@ import com.acme.server.component.PositionComponent;
 import com.acme.server.component.WorldComponent;
 import com.acme.server.packet.outbound.MovePacket;
 import com.acme.server.system.NetworkSystem;
-import com.acme.server.util.PositionUtils;
 import com.acme.server.world.Position;
 import com.acme.server.world.Region;
 import com.badlogic.ashley.core.ComponentMapper;
@@ -15,11 +14,13 @@ import com.badlogic.ashley.core.Entity;
 @Wired
 public class PositionManager extends ManagerSystem {
 
+    private ComponentMapper<PositionComponent> pcm;
     private ComponentMapper<WorldComponent> wcm;
+
     private NetworkSystem networkSystem;
 
     public void updatePosition(Entity entity, Position position) {
-        PositionComponent positionComponent = PositionUtils.pcm.get(entity);
+        PositionComponent positionComponent = pcm.get(entity);
         positionComponent.setPosition(position);
         updateRegion(entity, positionComponent.getPosition());
         networkSystem.sendToKnownList(entity, new MovePacket(entity));
@@ -27,7 +28,7 @@ public class PositionManager extends ManagerSystem {
 
     private void updateRegion(Entity entity, Position position) {
         WorldComponent worldComponent = wcm.get(entity);
-        PositionComponent positionComponent = PositionUtils.pcm.get(entity);
+        PositionComponent positionComponent = pcm.get(entity);
         Region oldRegion = positionComponent.getRegion();
         Region newRegion = worldComponent.getInstance().findRegion(position);
 
