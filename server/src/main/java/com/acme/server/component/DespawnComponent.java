643ac1f75fa7a5f4549337572582b792fc062664
bkorotaev <54k@bk.ru>
2015-02-03 00:13:55 +0500

    items blinking now

--- server/src/main/java/com/acme/server/component/DespawnComponent.java
+++ server/src/main/java/com/acme/server/component/DespawnComponent.java
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.Component;
 public class DespawnComponent extends Component {
 
     private float cooldown;
+    private boolean blinking;
 
     public float getCooldown() {
         return cooldown;
@@ -12,5 +13,13 @@ public class DespawnComponent extends Component {
 
     public void setCooldown(float cooldown) {
         this.cooldown = cooldown;
+    }
+
+    public boolean isBlinking() {
+        return blinking;
+    }
+
+    public void setBlinking(boolean blinking) {
+        this.blinking = blinking;
     }
 }
