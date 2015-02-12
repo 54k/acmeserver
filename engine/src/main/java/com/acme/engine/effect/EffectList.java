96b79545a861adf9d78d8cee68de6699a78d3d2b
Korotaev Boris <korotaev@multivi.ru>
2015-02-12 18:18:45 +0600

    effect system sketch

--- engine/src/main/java/com/acme/engine/effect/EffectList.java
+++ engine/src/main/java/com/acme/engine/effect/EffectList.java
@@ -3,7 +3,9 @@ package com.acme.engine.effect;
 import com.badlogic.ashley.core.Entity;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class EffectList {
 
@@ -11,24 +13,73 @@ public class EffectList {
 
     private final List<Effect> effects = new ArrayList<>();
 
+    private final Queue<Operation> operations = new LinkedList<>();
+    private boolean updating;
+
     public EffectList(Entity owner) {
         this.owner = owner;
     }
 
     public void apply(Effect effect) {
+        if (updating) {
+            operations.add(new Operation(OperationType.APPLY, effect));
+        } else {
+            apply0(effect);
+        }
+    }
+
+    private void apply0(Effect effect) {
         effects.add(effect);
         effect.apply(owner);
     }
 
     public void update(float deltaTime) {
-        ArrayList<Effect> effects = new ArrayList<>(this.effects);
+        executeOperations();
+        updating = true;
         for (Effect effect : effects) {
             effect.update(owner, deltaTime);
+        }
+        updating = false;
+    }
+
+    private void executeOperations() {
+        while (!operations.isEmpty()) {
+            Operation operation = operations.poll();
+            switch (operation.operationType) {
+                case APPLY:
+                    apply0(operation.effect);
+                    break;
+                case REMOVE:
+                    remove0(operation.effect);
+                    break;
+            }
         }
     }
 
     public void remove(Effect effect) {
+        if (updating) {
+            operations.add(new Operation(OperationType.REMOVE, effect));
+        } else {
+            remove0(effect);
+        }
+    }
+
+    private void remove0(Effect effect) {
         effects.remove(effect);
         effect.remove(owner);
+    }
+
+    private static final class Operation {
+        OperationType operationType;
+        Effect effect;
+
+        Operation(OperationType operationType, Effect effect) {
+            this.operationType = operationType;
+            this.effect = effect;
+        }
+    }
+
+    private static enum OperationType {
+        APPLY, REMOVE
     }
 }
